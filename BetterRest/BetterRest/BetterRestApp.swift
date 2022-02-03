baf7bf53d18d15e43989d1ad2257205b7a9d30c6
Dan Lovell <dan.lovell@lovcom.com>
2022-02-02 17:51:52 -0800

    proj 4

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dan Lovell on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
