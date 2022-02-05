c310962aef8da630efc12bc8fa445aed541417af
Dan Lovell <dan.lovell@lovcom.com>
2022-02-05 07:45:19 -0800

    prj5

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dan Lovell on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
