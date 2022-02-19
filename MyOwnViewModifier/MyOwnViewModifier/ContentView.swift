f177e93635f8924f289d480817d65d968ff49267
Dan Lovell <dan.lovell@lovcom.com>
2022-02-18 18:24:49 -0800

    2022-02-18

---
+++ MyOwnViewModifier/MyOwnViewModifier/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  MyOwnViewModifier
+//
+//  Created by Dan Lovell on 2/18/22.
+//
+
+import SwiftUI
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+                  identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
+struct ContentView: View {
+    @State private var isShowingRed: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+                .transition(.pivot)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyOwnViewModifier/MyOwnViewModifier/MyOwnViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOwnViewModifierApp.swift
+//  MyOwnViewModifier
+//
+//  Created by Dan Lovell on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MyOwnViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
