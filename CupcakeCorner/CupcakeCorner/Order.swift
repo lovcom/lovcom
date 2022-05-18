d43dc1e660b6c39c8e54227e572d9a6c23987d5d
Dan Lovell <dan.lovell@lovcom.com>
2022-05-18 05:53:15 -0700

    2022-05-18

--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -7,7 +7,27 @@
 
 import SwiftUI
 
-class Order: ObservableObject {
+class Order: ObservableObject, Codable {
+    
+    // *****************************************
+    // * Required for properties marked with   *
+    // * @Published property wrapper.          *
+    // * Enum defines properties to be Codable *
+    // * specialRequestEndabled property not   *
+    // * listed here because its a computed    *
+    // * value derived from the others.        *
+    // *****************************************
+    enum CodingKeys: CodingKey {
+        case type
+        case quantity
+        case extraFrosting
+        case addSprinkles
+        case name
+        case streetAddress
+        case city
+        case zip
+    }
+    
     static let types = ["Vanilla", "Chocolate", "Strawberry", "Rainbow"]
     
     @Published var type = 0
@@ -55,5 +75,49 @@ class Order: ObservableObject {
         
         return cost
     }
+    
+    // ****************************************************************************************
+    // * Called automatically by Swift to encode properties using property wrapper @Published *
+    // ****************************************************************************************
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+        
+    }
+    
+    // ******************************************************************
+    // * This instantiator is required to populate properties from JSON *
+    // ******************************************************************
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // **********************************************
+        // * Decoding the JSON can be done in any order *
+        // **********************************************
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+        name = try container.decode(String.self, forKey:    .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+        
+    }
+    
+    // ****************************************************************************
+    // * Since Order() is used in lots of places in this app, those places do not *
+    // * instantiate with a Decoder type, as is required in the init above.       *
+    // * For this reason, this simple init() is added:                            *
+    // ****************************************************************************
+    init() { }
     
 }
