46efdadda0faccd77aaaebcd9f1e62d31b1a99ef
Dan Lovell <dan.lovell@lovcom.com>
2022-05-18 18:02:30 -0700

    2022-05-18

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -26,13 +29,69 @@ struct CheckoutView: View {
                 Text("Your total cost \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place order", action: { })
-                    .padding()
+                Button("Place order") {
+                    Task { // Must wrap call to asynch inside Task
+                        await placeOrder()
+                    }
+                }
+                .padding()
             }
         }
         .navigationTitle("Check Out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
     }
+    
+    func placeOrder() async {
+        
+        // ********************************************************************
+        // * Encode order object into JSON and place JSON in constant encoded *
+        // ********************************************************************
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        // *************************************************************************************
+        // * Define Request URL. Request made to a generic testing server https://reqres.in... *
+        // * This testing server can be used for anything. It will return a response that is   *
+        // * the same as the request.                                                          *
+        // *************************************************************************************
+        let url = URL(string: "https://reqres.in/api/cupcakes")! // Force unpack optional
+        var request = URLRequest(url: url)
+        
+        // *****************************
+        // * Define HTTP Header Values *
+        // *****************************
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        // *********************************
+        // * Perform the REST POST Request *
+        // *********************************
+        do {
+            let (response, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            // ********************************************
+            // * Convert JSON Response to an Order object *
+            // ********************************************
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: response)
+            
+            // ******************************************************
+            // * Set confirmation message for an alert dialogue box *
+            // ******************************************************
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
+        
+    }
+    
     
 }
 
